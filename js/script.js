250302777f64ee97d764a868f2fe7531cbe52441
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-14 15:37:23 +0100

    array numeri random

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert('Hello')
+// alert('Hello')
+
+var numRandomArray = [];
+
+//------- GENERA 16 NUMERI RANDOM ----------------
+
+//            COL FOR
+
+for ( var i = 0; i <= 15; i++) {
+  var numbersRandom = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  console.log(numbersRandom);
+  numRandomArray.push(numbersRandom);
+}
+console.log(numRandomArray);
